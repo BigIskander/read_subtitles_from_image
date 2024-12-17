1c431a557cf1e31af4ad27a01d63309c7b9391ea
BigIskander <bigiskander@gmail.com>
2024-12-17 20:17:32 +0300

    Add code in development.

---
+++ index.js
@@ -0,0 +1,152 @@
+import * as THREE from 'three';
+
+var camera, scene, renderer; //, clock;
+var clicked = false;
+// var uniforms;
+
+// function load_shader(file_url) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             fetch(file_url).then(
+//                 (response) => response.text()).then((data) => { resolve(data); }
+//             );
+//         } catch(error) {
+//             reject(error);
+//         }
+//     });
+// }
+
+// var vertexShader = await load_shader("./shader.vert");
+// var fragmentShader = await load_shader("/shader.frag");
+
+var canvas = document.querySelector("#main_canvas");
+var circleMesh = [];
+var circleMeshRadius = 0.02;
+
+function init() {
+    camera = new THREE.Camera();
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+    // clock = new THREE.Clock();
+
+    var geometry = new THREE.PlaneGeometry(2, 2);
+    var circle = new THREE.CircleGeometry(circleMeshRadius);
+
+    // uniforms = {
+    //     u_time: { type: "f", value: 1.0 },
+    //     u_resolution: { type: "v2", value: new THREE.Vector2() },
+    //     u_mouse: { type: "v2", value: new THREE.Vector2() }
+    // };
+
+    // var material = new THREE.ShaderMaterial({
+    //     uniforms: uniforms,
+    //     vertexShader: vertexShader,
+    //     fragmentShader: fragmentShader
+    // });
+
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xcccccc }));
+    scene.add(mesh);
+    for(let i = 0; i < 4; i++) {
+        circleMesh[i] = new THREE.Mesh(circle, new THREE.MeshBasicMaterial({ color: 0xfff000 }));
+        circleMesh[i].position.x += -0.1 + (i % 2) * 0.2;
+        circleMesh[i].position.y += -0.1 + Math.floor(i / 2) * 0.2;
+        scene.add(circleMesh[i]);
+    }
+
+    canvas.addEventListener("mousemove", onCanvasMouse);
+    canvas.addEventListener("click", onCanvasClick);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: canvas
+    });
+    renderer.setPixelRatio( window.devicePixelRatio );
+
+    // onWindowResize();
+    // window.addEventListener('resize', onWindowResize, false);
+
+    // document.onmousemove = function(e){
+    //   uniforms.u_mouse.value.x = e.pageX
+    //   uniforms.u_mouse.value.y = window.innerHeight - e.pageY
+    // }
+
+    // renderer.setAnimationLoop(render);
+    render();
+}
+
+// function onWindowResize() {
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     uniforms.u_resolution.value.x = renderer.domElement.width;
+//     uniforms.u_resolution.value.y = renderer.domElement.height;
+// }
+
+function getXY(event) {
+    var x = (event.pageX / canvas.width) * 2.0 - 1.0;
+    var y = (event.pageY / canvas.height) * 2.0 - 1.0;
+    y *= -1; // flip y
+    return { x: x, y: y };
+}
+
+function isWithinCircle(event, circleID = 3) {
+    var xy = getXY(event);
+    // measure distance to the center of circle
+    var d = Math.pow(
+        Math.pow(circleMesh[circleID].position.x - xy.x, 2.0) 
+                    + Math.pow(circleMesh[circleID].position.y - xy.y, 2.0),
+        0.5
+    );
+    // if mouse inside the circle
+    if(d <= circleMeshRadius) return true;
+    return false;
+}
+
+function onCanvasMouse(event) {
+    if(clicked) {
+        var xy = getXY(event);
+        // do not go outside the bounderies
+        if(xy.x < circleMeshRadius - 1) xy.x = circleMeshRadius - 1;
+        if(xy.y < circleMeshRadius - 1) xy.y = circleMeshRadius - 1;
+        if(xy.x > 1 - circleMeshRadius) xy.x = 1 - circleMeshRadius;
+        if(xy.y > 1 - circleMeshRadius) xy.y = 1 - circleMeshRadius;
+        // set position
+        circleMesh.position.x = xy.x;
+        circleMesh.position.y = xy.y;
+    } else {
+        // if mouse inside the circle
+        if(isWithinCircle(event)) {
+            canvas.style.cursor = 'pointer';
+            circleMesh[3].material.color.setHex(0x000fff);
+        } else {
+            canvas.style.cursor = 'default';
+            circleMesh[3].material.color.setHex(0xfff000);
+        }
+    }
+    render();
+}
+
+function onCanvasClick(event) {
+    if(clicked) {
+        clicked = false;
+    } else {
+        if(isWithinCircle(event)) {
+            clicked = true;
+        }
+    }
+}
+
+function onMouseLeave(event) {
+    clicked = false;
+    canvas.style.cursor = 'default';
+    circleMesh.material.color.setHex(0xfff000);
+    render();
+}
+
+function render() {
+    // uniforms.u_time.value += clock.getDelta();
+    // circleMesh.position.x = 1;
+    renderer.render( scene, camera );
+}
+
+init();
