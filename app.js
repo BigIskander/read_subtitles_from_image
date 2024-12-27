405f5552ad0e6ff3a12e5f4fdff144cdbf41909a
BigIskander <bigiskander@gmail.com>
2024-12-27 09:27:59 +0300

    Add scripts.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.use(express.static('dist'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+  console.log(`http://localhost:${port}/`);
+});
