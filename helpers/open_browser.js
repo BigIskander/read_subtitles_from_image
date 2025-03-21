efde3186a4b4f0b90084f6b7d9a8c5d6f56877a5
BigIskander <bigiskander@gmail.com>
2025-03-21 05:16:16 +0300

    Make it more convenient.

--- helpers/open_browser.js
+++ helpers/open_browser.js
@@ -3,6 +3,8 @@ var openBrowsers = require('open-browsers');
 if(process.env.VITE == 'true') var port = 5173
 else var port = 3000
 
-if (openBrowsers(`http://localhost:${port}/`)) {
-  console.log('The browser tab has been opened!');
-}
+setTimeout(() => { 
+  if (openBrowsers(`http://localhost:${port}/`)) {
+    console.log('The browser tab has been opened!');
+  }
+}, 300);
